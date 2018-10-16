b7dbaf115b4c8f0634647c7bad39bbf4a630f13d
Johnny Ramos <johnny.gerardo24@hotmail.com>
2018-10-16 17:05:50 -0500

    login

---
+++ esgplay_db.sql
@@ -0,0 +1,111 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2018-10-16 00:31:42.579
+
+-- tables
+-- Table: Game
+CREATE TABLE Game (
+    id int  NOT NULL auto_increment,
+    name nvarchar(50)  NOT NULL,
+    CONSTRAINT Game_pk PRIMARY KEY  (id)
+);
+
+-- Table: Play
+CREATE TABLE Play (
+    id int  NOT NULL auto_increment,
+    game_id int  NOT NULL,
+    flag_victory int  NOT NULL,
+    play_date datetime  NOT NULL,
+    CONSTRAINT Play_pk PRIMARY KEY  (id)
+);
+
+-- Table: PlayDetails
+CREATE TABLE PlayDetails (
+    id int  NOT NULL auto_increment,
+    user_id int  NOT NULL,
+    play_id int  NOT NULL,
+    flag_team int  NOT NULL,
+    gpm int  NOT NULL,
+    xpm int  NOT NULL,
+    kills int  NOT NULL,
+    deaths int  NOT NULL,
+    assists int  NOT NULL,
+    kda decimal(4,2)  NOT NULL,
+    CONSTRAINT PlayDetails_pk PRIMARY KEY  (id)
+);
+
+-- Table: Team
+CREATE TABLE Team (
+    id int  NOT NULL auto_increment,
+    name nvarchar(255)  NOT NULL,
+    CONSTRAINT Team_pk PRIMARY KEY  (id)
+);
+
+-- Table: Tournament
+CREATE TABLE Tournament (
+    id int  NOT NULL auto_increment,
+    name nvarchar(255)  NOT NULL,
+    description text  NOT NULL,
+    first_day date  NOT NULL,
+    last_day date  NOT NULL,
+    max_teams int  NOT NULL,
+    url_to_image int  NOT NULL,
+    CONSTRAINT Tournament_pk PRIMARY KEY  (id)
+);
+
+-- Table: TournamentPlay
+CREATE TABLE TournamentPlay (
+    id int NOT NULL auto_increment,
+    tournament_id int  NOT NULL,
+    play_id int  NOT NULL,
+    scheduled date  NOT NULL,
+    first_team_score int  NULL,
+    second_team_score int  NULL,
+    CONSTRAINT TournamentPlay_pk PRIMARY KEY  (id)
+);
+
+-- Table: User
+CREATE TABLE User (
+    id int  NOT NULL auto_increment,
+    username nvarchar(255)  NOT NULL,
+    email nvarchar(255) NULL,
+    password nvarchar(255)  NOT NULL,
+    phone nvarchar(255) NULL,
+    steam_id int  NULL,
+    steam_name nvarchar(255)  NULL,
+    team_id int NULL,
+    coach bit NULL,
+    CONSTRAINT User_pk PRIMARY KEY  (id)
+);
+
+-- foreign keys
+-- Reference: PlayDetails_Play (table: PlayDetails)
+ALTER TABLE PlayDetails ADD CONSTRAINT PlayDetails_Play
+    FOREIGN KEY (play_id)
+    REFERENCES Play (id);
+
+-- Reference: PlayDetails_User (table: PlayDetails)
+ALTER TABLE PlayDetails ADD CONSTRAINT PlayDetails_User
+    FOREIGN KEY (user_id)
+    REFERENCES User (id);
+
+-- Reference: Play_Game (table: Play)
+ALTER TABLE Play ADD CONSTRAINT Play_Game
+    FOREIGN KEY (game_id)
+    REFERENCES Game (id);
+
+-- Reference: TournamentPlay_Play (table: TournamentPlay)
+ALTER TABLE TournamentPlay ADD CONSTRAINT TournamentPlay_Play
+    FOREIGN KEY (play_id)
+    REFERENCES Play (id);
+
+-- Reference: TournamentPlay_Tournament (table: TournamentPlay)
+ALTER TABLE TournamentPlay ADD CONSTRAINT TournamentPlay_Tournament
+    FOREIGN KEY (tournament_id)
+    REFERENCES Tournament (id);
+
+-- Reference: User_Team (table: User)
+ALTER TABLE User ADD CONSTRAINT User_Team
+    FOREIGN KEY (team_id)
+    REFERENCES Team (id);
+
+-- End of file.
--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ router.get('/',function(req, res){
     })
 });
 
-router.post('/init', function(req, res){
+router.post('/login', function(req, res){
     req.checkBody('username','Username is required').notEmpty();
     req.checkBody('password','Password is required').notEmpty();
 
@@ -17,13 +17,16 @@ router.post('/init', function(req, res){
     var password = req.body.password;
 
     if(errors){
-        req.flash('danger',errors);
-        res.redirect('/login');
+        //req.flash('danger',errors);
+        res.render('login',{
+            title: 'Login',
+            errors: errors
+        })
     }else{
         let query = 'SELECT * FROM user WHERE username = "'+ username + '" AND password = "'+ password + '"';
         db.query(query, function(err,result) {
-            if( err){
-                console.log(err);
+            if(err){console.log(err); return;}
+            if(result.length == 0){
                 req.flash('danger',"Incorrect username and password");
                 res.redirect('/login');
             }else{
@@ -31,6 +34,55 @@ router.post('/init', function(req, res){
             }
         })
     }
-})
+});
+
+router.post('/',function(req, res){
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('email', 'Email wrong format').isEmail();
+    req.checkBody('phone','Phone wrong format').isNumeric();
+    req.checkBody('steam_id','SteamID is required').notEmpty();
+    req.checkBody('password','Password is required').notEmpty();
+    req.checkBody('repeatpass','Confirm password is required').notEmpty();
+    //req.checkBody('confirm-info','You must confirm your information')
+
+    let errors = req.validationErrors();
+
+    var username = req.body.username;
+    var password = req.body.password;
+    var repeatpass = req.body.repeatpass;
+    var email = req.body.email;
+    var steam_id = req.body.steam_id;
+    var phone = req.body.phone;
+
+    if(errors){
+        res.render('login',{
+            title: 'Login',
+            errors: errors
+        })
+    }else{
+        let querySelect = 'SELECT * FROM user WHERE username = "'+username+'" OR email = "'+email+'";';
+        db.query(querySelect, function(err, result){
+            if(err){console.log(err); return;}
+            if(result.length > 0){
+                req.flash('danger','El usuario o email ya se encuentra registrado');
+                res.redirect('/login');
+            }else{
+                if(password !== repeatpass){
+                    req.flash('danger', 'Las constraseñas no coinciden');
+                    res.redirect('/login');
+                }else{
+                    let queryInsert = 'INSERT INTO user (username,password,email,steam_id,phone) VALUES ("'+username+'","'+password+'","'+email+'","'+steam_id+'","'+phone+'");';
+                    db.query(queryInsert, function(err, result){
+                        if(err){console.log(err); return;}
+                        if(result){
+                            req.flash('success','Bienvenido, usuario registrado');
+                            res.redirect('/login'); 
+                        }
+                    });
+                }
+            }
+        })
+    }
+});
 
 module.exports = router;

f6a5eae709a502da46406b1fd3ab611cad4caa48
rodcar <ivanrod12@hotmail.com>
2018-10-18 09:53:44 -0500

    Agrega archivo para ingresar datos de prueba
    Datos de prueba para la base de datos

---
+++ esgplay_db_insert.sql
@@ -0,0 +1,65 @@
+/* Datos de prueba */
+
+insert into user values (
+	null,
+    'ivanrod',
+    null,
+    '1234',
+    null,
+    null,
+    null,
+    null,
+    null
+);
+
+insert into game values (
+	null,
+    'Juego 1'
+);
+
+insert into play values (
+	null,
+    1,
+    false,
+    '2018-10-15'
+);
+
+insert into play values (
+	null,
+    1,
+    true,
+    '2018-10-15'
+);
+
+insert into play values (
+	null,
+    1,
+    true,
+    '2018-10-15'
+);
+
+insert into playdetails values (
+	null,
+    1,
+    1,
+    1,
+    10,
+    15,
+    5,
+    3,
+    3,
+    12.44
+);
+
+insert into PlayDetails values (
+	null,
+    1,
+    2,
+    1,
+    2,
+    7,
+    1,
+    4,
+    4,
+    5.24
+);

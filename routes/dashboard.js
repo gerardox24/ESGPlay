d9d361be8d232040f6cd1240100dcb3e32378a7c
rodcar <ivanrod12@hotmail.com>
2018-10-18 10:59:25 -0500

    Agrega mostrar estadisticas al dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',function(req, res) {
-    res.render('dashboard',{
-        title: 'Dashboard'
-    })
+	const user_id = req.session.user_id;
+	let sp = 'call sp_listar_estadisticas_usuario(' + user_id + ')';
+
+	db.query(sp, function(err,result) {
+		if(err){ console.log(err); return; }
+    	res.render('dashboard',{
+        	title: 'Dashboard',
+        	estadisticas: result[0]
+    	});
+    });
 })
 
 module.exports = router;

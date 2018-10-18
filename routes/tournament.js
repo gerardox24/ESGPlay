0696603f1248333f31c2b5c3abf563d385b21f65
rodcar <ivanrod12@hotmail.com>
2018-10-18 18:03:38 -0500

    Agrega listar los torneos

--- routes/tournament.js
+++ routes/tournament.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',function(req, res){
-    res.render('tournament',{
-        title: 'Torneos'
-    })
+	let sp = 'call sp_listar_torneos()';
+	db.query(sp, function(err, result){
+		let torneos = result[0];
+		res.render('tournament',{
+        	title: 'Torneos',
+        	tournaments: torneos
+    	});
+	});
 });
 
 module.exports = router;

47058a9aa3cdc3bb911c7610de84ebc136f61879
rodcar <ivanrod12@hotmail.com>
2018-10-22 17:44:53 -0500

    Agrega la actualización de la información de un torneo
    Cualquier usuario puede actualizar la información de un torneo, se debe validar que el usuario haya creado el torneo

--- routes/tournament.js
+++ routes/tournament.js
@@ -64,4 +64,48 @@ router.get('/:id', function(req, res) {
 	});
 });
 
+router.get('/edit/:id', function(req, res) {
+	const id = req.params.id;
+	const sp = 'call sp_listar_torneo(' + id + ')';
+
+	db.query(sp, function(err, result) {
+		if(err) { console.log(err); return;}
+		const torneo = result[0][0];
+		console.log(torneo);
+
+		res.render('tournament_edit', {
+			torneo: torneo
+		});
+	});
+});
+
+router.put('/:id', function(req, res) {
+	req.checkBody('nombre', 'El nombre del torneo es requerido').notEmpty();
+	req.checkBody('descripcion', 'La descripción del torneo es requerida').notEmpty();
+	req.checkBody('fecha_inicio', 'La fecha de inicio del torneo es requerida').notEmpty();
+	req.checkBody('fecha_fin', 'La fecha de fin del torneo es requerida').notEmpty();
+	req.checkBody('maximo_equipos', 'El número máximo de equipos del torneo es requerido').notEmpty();
+	req.checkBody('url_imagen', 'Una imagen para el torneo es requerida').notEmpty();
+
+	let errors = req.validationErrors();
+
+	if(!errors) {
+		const id = req.params.id;
+		const nombre = req.body.nombre;
+		const descripcion = req.body.descripcion;
+		const fecha_inicio = req.body.fecha_inicio;
+		const fecha_fin = req.body.fecha_fin;
+		const maximo_equipos = req.body.maximo_equipos;
+		const url_imagen = req.body.url_imagen;
+		let sp = 'call sp_actualizar_torneo(' + id + ',"' + nombre + '","' + descripcion +'","' + fecha_inicio +'","' + fecha_fin + '",' + maximo_equipos + ',"' + url_imagen + '")';
+		db.query(sp, function(err, result) {
+			if(err){ console.log(err); return; }
+			res.redirect('/tournament');
+		});
+	} else {
+		// TODO se puede mostrar los errores en lugar de redirigir
+		res.redirect('/tournament/edit/' + id);
+	}	
+});
+
 module.exports = router;

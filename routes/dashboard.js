a77dcc08946d57bc478e4d23cc0a095f0132f24e
Ivan Rodriguez <ivanrod12@hotmail.com>
2018-11-20 17:40:18 -0500

    Arregla mostrar las estadísticas

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,19 +5,14 @@ router.get('/',function(req, res) {
 	const user_id = req.session.user_id;
 	let sp = 'call sp_listar_estadisticas_usuario(' + user_id + ')';
 
-	// db.query(sp, function(err,result) {
-	// 	if(err){ console.log(err); return; }
-    // 	res.render('dashboard',{
-    //     	title: 'Dashboard',
-	// 		estadisticas: result[0],
-	// 		session: user_id
-    // 	});
-	// });
-	
-	res.render('dashboard',{
-		title: 'Dashboard',
-		estadisticas: [],
-		session: user_id
+	db.query(sp, function(err,result) {
+		if(err){ console.log(err); return; }
+		console.log(result);
+		res.render('dashboard',{
+	    	title: 'Dashboard',
+			estadisticas: result[0],
+			session: user_id
+		});
 	});
 })
 
